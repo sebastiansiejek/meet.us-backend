91345d2a6624497be0209215e0bc91d1c874e2ea
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-04-28 19:03:46 +0200

    chore: configure i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,21 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UsersModule } from './users/users.module';
 import { MailModule } from './mail/mail.module';
+import { I18nModule, I18nJsonParser } from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      parser: I18nJsonParser,
+      parserOptions: {
+        path: path.join(__dirname, 'i18n'),
+        watch: true,
+      },
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
@@ -22,6 +32,8 @@ import { MailModule } from './mail/mail.module';
       keepConnectionAlive: true,
     }),
     GraphQLModule.forRoot({
+      context: ({ req, connection }) =>
+        connection ? { req: connection.context } : { req },
       autoSchemaFile: true,
       sortSchema: true,
     }),
