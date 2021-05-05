13442886167e59d7d9182ee7a964146e0896b738
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-05-05 19:54:24 +0200

    fix: remove context.getType() conditional

--- src/auth/current-user.decorator.ts
+++ src/auth/current-user.decorator.ts
@@ -3,11 +3,8 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const CurrentUser = createParamDecorator(
   (_data: unknown, context: ExecutionContext) => {
-    if (context.getType() === 'http') {
-      return context.switchToHttp().getRequest().user;
-    }
-
     const ctx = GqlExecutionContext.create(context);
+
     return ctx.getContext().req.user;
   },
 );
