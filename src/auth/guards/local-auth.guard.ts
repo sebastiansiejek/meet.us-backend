3c77dd666939cee811ba7890d436317e3c34c674
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-05-05 19:46:17 +0200

    style: local-auth.guard

--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -6,7 +6,9 @@ export class LocalAuthGuard extends AuthGuard('local') {
   getRequest(context: ExecutionContext): Request {
     const ctx = GqlExecutionContext.create(context);
     const { req } = ctx.getContext();
+
     req.body = ctx.getArgs().loginUserInput;
+
     return req;
   }
 }
