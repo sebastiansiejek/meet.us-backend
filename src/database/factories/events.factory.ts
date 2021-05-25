4ec3d1e8fd60a0392508dbe24a0f245986aa479a
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-05-25 18:30:15 +0200

    feat: generate days from past, today and future

--- src/database/factories/events.factory.ts
+++ src/database/factories/events.factory.ts
@@ -2,7 +2,11 @@ import Faker from 'faker';
 import { define, factory } from 'typeorm-seeding';
 import { Event, eventType, state } from 'src/events/entities/event.entity';
 import { User } from 'src/users/entities/user.entity';
-import { getRandomKeyFromObject } from 'utils/getRandoms';
+import {
+  getRandomDateFromDate,
+  getRandomKeyFromObject,
+  getRandomValueFromArray,
+} from 'utils/getRandoms';
 
 define(Event, (faker: typeof Faker) => {
   const event = new Event();
@@ -11,8 +15,20 @@ define(Event, (faker: typeof Faker) => {
   event.description = faker.lorem.sentence();
   event.type = getRandomKeyFromObject(eventType);
   event.state = getRandomKeyFromObject(state);
-  event.startDate = faker.date.future();
-  event.endDate = faker.date.future();
+
+  const today = new Date();
+  const dates = [
+    faker.date.past(),
+    today,
+    getRandomDateFromDate(today, 3, 20).endDate,
+  ];
+  const { startDate, endDate } = getRandomDateFromDate(
+    getRandomValueFromArray(dates),
+  );
+
+  event.startDate = startDate;
+  event.endDate = endDate;
+
   event.maxParticipants = faker.random.number(100);
   event.user = factory(User)() as any;
 
