a45de4f7e318f406660eb1bb51cdabfe0c3c71de
Dawid Smalc <dawid.smalc@gmail.com>
2021-06-03 13:50:20 +0200

    feat: import event, user add image controller

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Image } from './entities/image.entity';
 import { ImagesService } from './images.service';
-import { ImagesResolver } from './images.resolver';
+import { ImagesController } from './images.controller';
+import { UsersModule } from 'src/users/users.module';
+import { EventsModule } from 'src/events/events.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Image])],
-  providers: [ImagesService, ImagesResolver],
+  imports: [TypeOrmModule.forFeature([Image]), UsersModule, EventsModule],
+  providers: [ImagesService],
+  controllers: [ImagesController],
 })
 export class ImagesModule {}
