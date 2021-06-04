d0997d4c163a25982936cf3a4bd62451b24b6889
skwarecky <dawid.smalc@gmail.com>
2021-06-04 13:42:37 +0200

    feat: add auth guard

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -3,6 +3,8 @@ import {
   Post,
   UseInterceptors,
   UploadedFile,
+  UseGuards,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
@@ -10,6 +12,11 @@ import { extname } from 'path';
 import { ImagesService } from './images.service';
 import { UsersService } from '../users/users.service';
 import { EventsService } from 'src/events/events.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/current-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { I18nRequestScopeService } from 'nestjs-i18n';
+import { Args } from '@nestjs/graphql';
 
 @Controller('images')
 export class ImagesController {
@@ -17,7 +24,9 @@ export class ImagesController {
     private readonly imageService: ImagesService,
     private readonly userService: UsersService,
     private readonly eventService: EventsService,
+    private readonly i18n: I18nRequestScopeService,
   ) {}
+  @UseGuards(JwtAuthGuard)
   @Post('upload-avatar')
   @UseInterceptors(
     FileInterceptor('file', {
@@ -33,15 +42,24 @@ export class ImagesController {
       }),
     }),
   )
-  async uploadAvatarImage(userId: string, @UploadedFile() file) {
+  async uploadAvatarImage(@CurrentUser() user: User, @UploadedFile() file) {
+    const userDetails = await this.userService.findOne(user.id);
+    if (userDetails.id != user.id) {
+      throw new UnauthorizedException(
+        await this.i18n.translate('errors.ERRORS.USER_IS_NOT_OWNER_OF_ACCOUNT'),
+      );
+    }
+
     const image = await this.imageService.saveImage(
       file.filename,
-      file.replace(/(public\\)/m, ''),
+      file.path.replace(/(public\\)/m, ''),
     );
-    this.userService.addImage(userId, image);
+    this.userService.addImage(user.id, image);
+
     return image;
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('upload-event-cover')
   @UseInterceptors(
     FileInterceptor('file', {
@@ -57,7 +75,19 @@ export class ImagesController {
       }),
     }),
   )
-  async uploadEventCover(eventId: string, @UploadedFile() file) {
+  async uploadEventCover(
+    @CurrentUser() user: User,
+    @Args('eventId') eventId: string,
+    @UploadedFile() file,
+  ) {
+    const eventDetails = await this.eventService.findOne(eventId);
+
+    if (eventDetails.user.id != user.id) {
+      throw new UnauthorizedException(
+        await this.i18n.translate('errors.ERRORS.USER_IS_NOT_OWNER_OF_EVENT'),
+      );
+    }
+
     const image = await this.imageService.saveImage(
       file.filename,
       file.path.replace(/(public\\)/m, ''),
