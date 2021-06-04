19d49da5236cc6866ecfc515a193fa82c84268ac
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-06-04 10:25:12 +0200

    feat: set fields in update

--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,8 +1,37 @@
 import { CreateUserInput } from './create-user.input';
 import { InputType, Field, PartialType } from '@nestjs/graphql';
+import { IsEmail, Length, Matches } from 'class-validator';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field()
+  @Field({
+    nullable: true,
+  })
   id: string;
+  @Field({
+    nullable: true,
+  })
+  @IsEmail()
+  @Length(4, 50)
+  email: string;
+  @Field({
+    nullable: true,
+  })
+  firstName: string;
+  @Field({
+    nullable: true,
+  })
+  @Length(8, 20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password should contains a capital letter and a number',
+  })
+  password: string;
+  @Field({
+    nullable: true,
+  })
+  lastname: string;
+  @Field({
+    nullable: true,
+  })
+  nickname: string;
 }
