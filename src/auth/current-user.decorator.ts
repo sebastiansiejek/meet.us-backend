b6f5d08ca5883606f1b8f5403c94997542cee08c
Dawid Smalc <dawid.smalc@gmail.com>
2021-06-04 10:18:24 +0200

    feat: add http request get current user

--- src/auth/current-user.decorator.ts
+++ src/auth/current-user.decorator.ts
@@ -3,8 +3,11 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const CurrentUser = createParamDecorator(
   (_data: unknown, context: ExecutionContext) => {
-    const ctx = GqlExecutionContext.create(context);
+    if (context.getType() === 'http') {
+      return context.switchToHttp().getRequest().user;
+    }
 
+    const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req.user;
   },
 );
