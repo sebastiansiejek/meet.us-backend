830bfb4fe370bc706bc1621aeb8741cbd533c2ff
skwarecky <dawid.smalc@gmail.com>
2021-07-10 18:04:44 +0200

    feat: add not active user exception

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,18 +1,26 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { I18nService } from 'nestjs-i18n';
 import { Strategy } from 'passport-local';
 import { User } from 'src/users/entities/user.entity';
 import { AuthService } from '../auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly i18n: I18nService,
+  ) {
     super({ usernameField: 'email' });
   }
 
   async validate(email: string, pass: string): Promise<User> {
     const user = await this.authService.validate(email, pass);
-
+    if (!user.isActive) {
+      throw new UnauthorizedException(
+        await this.i18n.translate('errors.ERROR.USER_NOT_ACTIVE'),
+      );
+    }
     if (!user) {
       throw new UnauthorizedException();
     }
