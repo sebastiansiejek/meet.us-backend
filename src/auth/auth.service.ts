cf71e16d1449f23a7890fe62d4a0debbc9cd6044
skwarecky <dawid.smalc@gmail.com>
2021-08-19 21:53:01 +0200

    feat: handle token refresh

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import * as crypto from 'crypto';
 import { User } from '../users/entities/user.entity';
 import { I18nService } from 'nestjs-i18n';
 
@@ -34,22 +35,70 @@ export class AuthService {
     }
   }
 
-  login(usersRepository: User): { accessToken: string } {
+  async login(
+    usersRepository: User,
+  ): Promise<{ accessToken: string; refreshToken: string }> {
+    const token = this.jwtService.sign({
+      email: usersRepository.email,
+      id: usersRepository.id,
+    });
+    const decoded = await this.verify(token);
+    return {
+      accessToken: token,
+      refreshToken: await this.createRefreshToken(decoded.id),
+    };
+  }
+
+  async refreshLoginToken(user: User, token: string) {
+    const searchedUser = await this.userService.findOne(user.id);
+    console.log(searchedUser);
+
+    if (searchedUser === undefined) {
+      throw new BadRequestException(
+        await this.i18n.translate('errors.ERROR.USER_NOT_FOUND'),
+      );
+    }
+
+    if (searchedUser.refreshTokenExpires < new Date()) {
+      throw new BadRequestException(
+        await this.i18n.translate('errors.ERROR.EXPIRED_TOKEN'),
+      );
+    }
+    if (await bcrypt.compare(token, searchedUser.refreshToken)) {
+      throw new BadRequestException(
+        await this.i18n.translate('errors.ERROR.TOKEN_NOT_FOUND'),
+      );
+    }
+
     return {
       accessToken: this.jwtService.sign({
-        email: usersRepository.email,
-        id: usersRepository.id,
+        email: searchedUser.email,
+        id: searchedUser.id,
       }),
+      refreshToken: await this.createRefreshToken(searchedUser.id),
     };
+  }
+
+  async createRefreshToken(userId: string): Promise<string> {
+    return await this.userService.saveRefreshToken(
+      userId,
+      await this.randomTokenString(),
+      new Date(new Date().getTime() + 2 * 24 * 60 * 60 * 1000),
+    );
   }
 
   async verify(token: string): Promise<User> {
     const decoded = this.jwtService.verify(token, {
       secret: process.env.JWT_SECRET,
     });
-
     const user = this.userService.findByMail(decoded.email);
-
     return user;
+  }
+
+  async randomTokenString(): Promise<{ raw: string; encryptedToken: string }> {
+    const randomString = crypto.randomBytes(100).toString('hex');
+    const salt = await bcrypt.genSalt(10);
+    const token = await bcrypt.hash(randomString, salt);
+    return { raw: randomString, encryptedToken: token };
   }
 }
