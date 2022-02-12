00faa73a5a21c9e21dc69d9969d8db622d95f450
skwarecky <dawid.smalc@gmail.com>
2022-02-12 18:21:38 +0100

    fix: notification module change

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,19 +1,17 @@
 import { UsersModule } from 'src/users/users.module';
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsResolver } from './events.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './entities/event.entity';
 import { EventAddress } from './entities/event-address.entity';
 import { UserActivityModule } from 'src/user-activity/user-activity.module';
-import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Event, EventAddress]),
     UserActivityModule,
     UsersModule,
-    forwardRef(() => NotificationsModule),
   ],
   providers: [EventsResolver, EventsService],
   exports: [EventsService],
--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,10 +1,11 @@
+import { EventsModule } from 'src/events/events.module';
 import { ParticipantsModule } from 'src/participants/participants.module';
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  imports: [ParticipantsModule, MailModule],
+  imports: [ParticipantsModule, MailModule, EventsModule],
   providers: [NotificationsService],
   exports: [NotificationsService],
 })
