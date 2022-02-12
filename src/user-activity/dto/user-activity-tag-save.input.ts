c350b399f3a08a6bf0173f39ee1f4ee36387525f
skwarecky <dawid.smalc@gmail.com>
2022-02-12 16:46:42 +0100

    feat: calculate new score

--- src/user-activity/dto/user-activity-tag-save.input.ts
+++ src/user-activity/dto/user-activity-tag-save.input.ts
@@ -1,10 +1,9 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { eventType } from 'src/events/entities/event.entity';
 import { User } from 'src/users/entities/user.entity';
 import { actionType } from '../entities/userActivity.entity';
 
 @ObjectType()
-export default class UserActivitySave {
+export default class UserActivityTagSave {
   @Field(() => User)
   user: User;
 
@@ -12,7 +11,7 @@ export default class UserActivitySave {
   actionType: actionType;
 
   @Field()
-  eventType: eventType;
+  tag: string;
 
   @Field()
   count: number;
